861fcc725550c1e66333d2554f77820eed3aa5fb
GrantLudwig <grantludwig97@gmail.com>
2019-10-21 13:28:34 -0700

    program1 made

--- ludwig_grant_AS11.py
+++ ludwig_grant_AS11.py
@@ -8,3 +8,18 @@
 # Midterm Pot Pourri
 
 # Program 1
+weekMessage = { "Monday" : "The Way Get Started Is To Quit Talking And Begin Doing.",
+				"Tuesday" : "The Pessimist Sees Difficulty In Every Opportunity. The Optimist Sees Opportunity In Every Difficulty.",
+				"Wednesday" : "Do Not Let Yesterday Take Up Too Much Of Today.",
+				"Thursday" : "You Learn More From Failure Than From Success. Don’t Let It Stop You. Failure Builds Character.",
+				"Friday" : "It Is Not Whether You Get Knocked Down, It Is Whether You Get Up.",
+				"Saturday" : "If You Are Working On Something That You Really Care About, You Do Not Have To Be Pushed. The Vision Pulls You.",
+				"Sunday" : "People Who Are Crazy Enough To Think They Can Change The World, Are The Ones Who Do."}
+				
+def encourageMe(day):
+	if day in weekMessage:
+		return weekMessage[day]
+	else:
+		return "You entered not a day of the week"
+		
+print(encourageMe("Friday"))
