67a07cac6e018aebd0cb9ba8d3b0bd0ec6824567
GrantLudwig <grantludwig97@gmail.com>
2019-10-21 13:05:53 -0700

    initial commit

---
+++ ludwig_grant_AS11.py
@@ -0,0 +1,10 @@
+# File Name: ludwig_grant_AS11.py
+# File Path: /home/ludwigg/Python/PyRpi_AS12/ludwig_grant_AS11.py
+# Run Command: sudo python3 /home/ludwigg/Python/PyRpi_AS12/ludwig_grant_AS11.py
+
+# Grant Ludwig
+# 10/25/2019
+# AS.11
+# Midterm Pot Pourri
+
+# Program 1
