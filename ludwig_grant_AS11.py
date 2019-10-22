c4a089d0156b2df20111f39003d73f1f4c0ad758
unknown <grantludwig97@gmail.com>
2019-10-21 19:39:30 -0700

    Program 5 complete, clearer test

--- ludwig_grant_AS11.py
+++ ludwig_grant_AS11.py
@@ -14,7 +14,7 @@ import random
 weekMessage = { "Monday" : "The Way Get Started Is To Quit Talking And Begin Doing.",
 				"Tuesday" : "The Pessimist Sees Difficulty In Every Opportunity. The Optimist Sees Opportunity In Every Difficulty.",
 				"Wednesday" : "Do Not Let Yesterday Take Up Too Much Of Today.",
-				"Thursday" : "You Learn More From Failure Than From Success. Don’t Let It Stop You. Failure Builds Character.",
+				"Thursday" : "You Learn More From Failure Than From Success. Do Not Let It Stop You. Failure Builds Character.",
 				"Friday" : "It Is Not Whether You Get Knocked Down, It Is Whether You Get Up.",
 				"Saturday" : "If You Are Working On Something That You Really Care About, You Do Not Have To Be Pushed. The Vision Pulls You.",
 				"Sunday" : "People Who Are Crazy Enough To Think They Can Change The World, Are The Ones Who Do."}
@@ -24,14 +24,33 @@ def encourageMe(day):
 		return weekMessage[day]
 	else:
 		return "You entered not a day of the week"
-		
-print(encourageMe("Friday"))
+
+print()
+print("Program 1")
+print("---------")	
+print("Monday:", encourageMe("Monday"))
+print("Tuesday:", encourageMe("Tuesday"))
+print("Wednesday:", encourageMe("Wednesday"))
+print("Thursday:", encourageMe("Thursday"))
+print("Friday:", encourageMe("Friday"))
+print("Saturday:", encourageMe("Saturday"))
+print("Sunday:", encourageMe("Sunday"))
+print()
+input("Press enter to continue")
+print()
 
 # Program 2
 def areaCalcRect(length, width):
 	return length * width
 	
-print(areaCalcRect(5, 10))
+print("Program 2")
+print("---------")	
+print("Length: 5, Width: 10 Area:", areaCalcRect(5, 10))
+print("Length: 100, Width: 30 Area:", areaCalcRect(100, 30))
+print("Length: 17, Width: 81 Area:", areaCalcRect(17, 81))
+print()
+input("Press enter to continue")
+print()
 
 # Program 3
 numberList = [5, 4, 3, 2, 1]
@@ -43,8 +62,13 @@ def printDecendingNumbers():
 			printString += str(num) + " "
 		print(printString)
 		del numberList[0]
-		
+
+print("Program 3")
+print("---------")		
 printDecendingNumbers()
+print()
+input("Press enter to continue")
+print()
 
 # Program 4
 colors = ["Red",
@@ -74,42 +98,34 @@ def main():
 	win.getMouse()
 	win.close()
 
+print("Program 4")
+print("---------")
 main()
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+print()
+input("Press enter to continue")
+print()
+
+# Program 5
+def stringReverse(strReverse):
+    returnString = ""
+    for i in range(-1, -len(strReverse) - 1, -1):
+        returnString += strReverse[i]
+    return returnString
+    
+print("Program 5")
+print("---------")
+print("Hello there.:", stringReverse("Hello there."))
+print("General Kenobi!:", stringReverse("General Kenobi!"))
+
+
+
+
+
+
+
+
+
+
+
+
+
